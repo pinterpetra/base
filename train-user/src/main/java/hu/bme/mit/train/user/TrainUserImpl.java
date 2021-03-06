32520aa1fa0db5324521d8e080ed4621cd54205c
Petra Pintér <pinterpetra@edu.bme.hu>
2021-03-06 16:03:22 +0100

    Gave id to TrainUserImpl

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -7,6 +7,7 @@ public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private int id;
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
