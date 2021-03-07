106ece3816482e3ef02e148c94fb23bb653eda2d
Petra Pintér <pinterpetra@edu.bme.hu>
2021-03-07 23:33:40 +0100

    tachographTest

---
+++ tachograph/src/test/java/hu/mit/bme/train/tachograph/TachographTest.java
@@ -0,0 +1,13 @@
+package hu.mit.bme.train.tachograph;
+
+import hu.bme.mit.train.tachograph.Tachograph;
+import org.junit.Before;
+
+public class TachographTest {
+    Tachograph t;
+
+    @Before
+    public void before() {
+        t = new Tachograph();
+    }
+}
