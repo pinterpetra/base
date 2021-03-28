8992445313e36fbfa64100c911926949b67de711
Petra Pintér <pinterpetra@edu.bme.hu>
2021-03-28 17:28:59 +0200

    2.2 Unit testing the new feature - fix issue

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,6 +22,7 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		user.setAlarmState(false);
 		if (speedLimit<0 || speedLimit>500) {
 			user.setAlarmState(true);
 		}
